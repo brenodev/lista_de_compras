ace965426ada8c9c40df7b1a0845dd3081c18cec
Breno G S Lopes <breno.souza.developer@gmail.com>
2019-11-01 17:31:10 -0300

    criado theme com material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { createMuiTheme, MuiThemeProvider  } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#009688'
+    },
+    secondary: {
+      main: '#834bff',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>APP</h1>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <h1>APP</h1>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
