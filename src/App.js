40b29bf3663ede0c220672ecd0dc4cad068bd4cc
Breno G S Lopes <breno.souza.developer@gmail.com>
2019-11-01 19:50:00 -0300

    criado header e adicionado font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createMuiTheme, MuiThemeProvider  } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+
+import "./App.css"
 
 const theme = createMuiTheme({
   palette: {
@@ -15,6 +18,11 @@ const theme = createMuiTheme({
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
+    <AppBar position="static" color="primary">
+        <div className="header">
+          <p className="title">Lista de compras</p>
+        </div>
+    </AppBar>
       <div className="App">
         <h1>APP</h1>
       </div>
