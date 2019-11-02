8ae4190cd5e74782954d8b62537c5a7c7bbbf470
Breno G S Lopes <breno.souza.developer@gmail.com>
2019-11-01 22:59:25 -0300

    adicionado interface de card na page home

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,22 @@
 import React from "react"
+import { Card, CardActionArea, CardContent } from '@material-ui/core';
+
+import "./Home.css"
 
 const Home = props => {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="page-container">
+    <div className="new-list-container">
+      <Card className="card">
+        <CardActionArea className="card-action-area">
+          <CardContent className="card-content">
+            <div>
+              <p className="title">Adicionar novas listas</p>
+            </div>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
     </div>
   )
 }
