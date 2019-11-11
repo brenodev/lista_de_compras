0926441b8ec7390b0ba4b4456e88f8d44275d089
Breno G S Lopes <breno.souza.developer@gmail.com>
2019-11-11 10:53:04 -0300

    otimizando o selector usando o Reselect

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import "./Home.css"
 import NewList from "./newList/NewList"
 import List from "./list/List"
+import { getListTotal } from "../home/list/reducer/list"
 
 import { connect } from "react-redux"
 
@@ -21,7 +22,7 @@ const Home = props => {
 }
 const mapStateToProps = state => ({
   list: state.list,
-  total: state.list.items.reduce((total, item) => total + item.total, 0),
+  total: getListTotal(state),
 });
 
 export default connect(mapStateToProps, null)(Home);
