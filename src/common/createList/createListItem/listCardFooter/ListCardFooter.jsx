8d151d23f0b41323b3dd6822a10739bd9b5e60e4
Breno G S Lopes <breno.souza.developer@gmail.com>
2019-11-11 23:41:42 -0300

    criado Types e função para deletar produto e adicionado uuid para gerar id unico para os produtos

--- src/common/createList/createListItem/listCardFooter/ListCardFooter.jsx
+++ src/common/createList/createListItem/listCardFooter/ListCardFooter.jsx
@@ -1,6 +1,10 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons"
+import {connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import { Creators as ListActions } from "../../../../home/list/actions/list"
+
 import "./ListCardFooter.css"
 
 const ListCardFooter = (props) => {
@@ -16,11 +20,19 @@ const ListCardFooter = (props) => {
         icon={faTrash}
         color="#834bff"
         size="sm"
+        onClick={() => props.deleteProduct(props.item.id)}
         />
       </div>
-      <p>Total: R$ {props.total}</p>
+      <p>Total: R$ {props.item.total}</p>
     </div>
   )
 }
 
-export default ListCardFooter
+const mapStateToProps = state => ({
+  list: state.list  
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(ListActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListCardFooter)
